cd2e25978947e336f1bea93e5638ad27bb150317
mohammadmahdihn <mohammadmahdiheydarinasab@gmail.com>
2024-12-16 22:39:54 +0330

    refactor

--- middleware/auth.js
+++ middleware/auth.js
@@ -3,20 +3,20 @@ const SECRET_KEY = 'secretKey';
 
 function authenticateToken(req, res, next) {
     const token = req.header('Authorization');
-    if (!token) return res.status(401).json({ message: 'Token does not exist' });
+    if (!token) return res.status(401).json({ message: 'You are not authenticated, Access Denied' });
 
     try {
         req.user = jwt.verify(token, SECRET_KEY);
         next();
     } catch (error) {
-        res.status(403).json({ message: 'Invalid token' });
+        res.status(403).json({ message: 'Invalid authentication token, Access Denied' });
     }
 }
 
 function authorizeRole(requiredRole) {
     return (req, res, next) => {
         if (req.user.role !== requiredRole) {
-            return res.status(403).json({ message: 'Access denied' });
+            return res.status(403).json({ message: 'You are not Authorized, Access denied' });
         }
         next();
     };
--- routes/questions.js
+++ routes/questions.js
@@ -2,16 +2,14 @@ const express = require('express');
 const router = express.Router();
 const { authenticateToken, authorizeRole } = require('../middleware/auth');
 const Question = require('../models/Question');
-const { questions } = require('../models/data'); // ایمپورت questions
+const { questions } = require('../models/data');
 
-// ایجاد سوال (فقط برای طراحان)
 router.post('/', authenticateToken, authorizeRole('designer'), (req, res) => {
     const { id, text, options, correctOption, difficulty } = req.body;
     questions.push(new Question(id, text, options, correctOption, difficulty, req.user.id));
-    res.status(201).json({ message: 'سوال ایجاد شد' });
+    res.status(201).json({ message: 'Question successfully created!' });
 });
 
-// دریافت همه سوالات
 router.get('/', authenticateToken, (req, res) => {
     res.json(questions);
 });
