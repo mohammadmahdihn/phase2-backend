4b8c1f7876907bacda2b36a197f73778bcca188d
mohammadmahdihn <mohammadmahdiheydarinasab@gmail.com>
2024-12-18 22:46:04 +0330

    add sumn

--- routes/answers.js
+++ routes/answers.js
@@ -4,12 +4,18 @@ const { authenticateToken, authorizeRole } = require('../middleware/auth');
 const Answer = require('../models/Answer');
 const { questions, answers, users } = require('../models/data');
 
-
 router.post('/', authenticateToken, authorizeRole('player'), (req, res) => {
     const { questionId, selectedOption } = req.body;
     const question = questions.find((q) => q.id === questionId);
-    if (!question) return res.status(404).json({ message: 'Question not found' });
-
+    if (!question) {
+        return res.status(404).json({ message: 'Question not found' });
+    }
+    const existingAnswer = answers.find(
+        (a) => a.questionId === questionId && a.playerId === req.user.id
+    );
+    if (existingAnswer) {
+        return res.status(400).json({ message: 'You have already answered this question.' });
+    }
     const isCorrect = question.correctOption === selectedOption;
     const answer = new Answer(questionId, req.user.id, selectedOption, isCorrect);
     const user = users.find((u) => u.id === req.user.id);
@@ -22,6 +28,7 @@ router.post('/', authenticateToken, authorizeRole('player'), (req, res) => {
         user.score -= 1;
     }
     answers.push(answer);
+
     res.json({ message: isCorrect ? 'Correct answer' : 'Wrong answer', isCorrect });
 });
 
